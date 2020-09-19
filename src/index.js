d859f3e2c56687a7f7e1194166c93a7119a2f5c6
Valeria <valery0007@gmail.com>
2020-09-19 19:19:38 +0200

    promises

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import { fetchWithTimeout } from "./services";
+import { fetchMovies, fetchBooks } from "./services";
 
 const movies = require("./data/movies.json");
 
-export function fetchMovies() {
+/*export function fetchMovies() {
   const resolveFunction = () => movies;
 
   return fetchWithTimeout(1000).then(resolveFunction);
-}
+}*/
 
-let moviePromise = fetchMovies();
+/*let moviePromise = fetchMovies();
 
 moviePromise.then((results) => {
   console.log(results);
 });
+*/
+
+ getBooksAndMovies() {
+  return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch((error) => console.log("Error fetching books and movies", error));
+}
+
+ getBooksOrMovies() {
+ return Promise.race([fetchBooks(), fetchMovies()]).then(results => results).catch(error => console.log("Error waiting for the promise race"));
+ }
+ 
+ const getBooksOrMoviesPromise = getBooksOrMovies();
+ 
+ getBooksOrMoviesPromise.then(results, console.log('getBooksOrMoviesPromise', results));
+ 
+ 
+ 
+ 
+ 

1f50f79d2928b1f57fe66e0c889044b5fbed0394
Valeria <valery0007@gmail.com>
2020-09-26 19:57:34 +0200

    fix error

--- src/index.js
+++ src/index.js
@@ -9,18 +9,22 @@ const getBooksAndMovies = () => {
     return Promise.all([fetchBooks(), fetchMovies()])
         .then(([books, movies]) => ({
             books,
-            movies,
+            movies
         }))
-        .catch((error) => console.log("Error fetching books and movies", error));
+        .catch(error => {
+            console.log("Error fetching books and movies", error)
+        });
 }
 
 const getBooksAndMoviesPromise = getBooksAndMovies();
-getBooksAndMoviesPromise.then((results) => {
+getBooksAndMoviesPromise.then(results => {
     console.log('getBooksAndMoviesPromise', results);
 });
 
-getBooksOrMovies() {
-    return Promise.race([fetchBooks(), fetchMovies()]).then(results => results).catch(error => console.log("Error waiting for the promise race", error));
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()]).then(results => results).catch(error => {
+        console.log("Error waiting for the promise race", error)
+    });
 }
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {

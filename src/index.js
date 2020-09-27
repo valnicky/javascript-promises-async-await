ed1fcfa9c45128274fe4d0b1b12772d0e5f25f4f
Valeria <valery0007@gmail.com>
2020-09-27 16:44:32 +0200

    error

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-    const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
     return values;
     /*try {
         const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
